e6ac6c8d19bd6b627505e2f631f77a471e1bcdcc
EpiXl33t <ryan.lee@argusinsights.com>
2013-06-10 15:55:40 -0700

    fixed select statment

--- src/LocationMapper/SQLConnection.java
+++ src/LocationMapper/SQLConnection.java
@@ -25,9 +25,9 @@ public class SQLConnection
 	
 	public final String statement = "" +
 	"SELECT id, interaction_geo_latitude, interaction_geo_longitude, twitter_user_location, twitter_user_lang " +
-	//"FROM datasift_results " +
+	"FROM datasift_results " +
 	"WHERE id > 33900000 " +
-	"WHERE country is null " + 
+	//"WHERE country is null " + 
 	"AND (twitter_user_location is not null or interaction_geo_latitude is not null)";
 
 	

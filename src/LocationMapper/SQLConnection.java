88d902487726e64a8295da903d085c7960afdb0b
EpiXl33t <ryan.lee@argusinsights.com>
2013-07-10 13:30:35 -0700

    test for ale

--- src/LocationMapper/SQLConnection.java
+++ src/LocationMapper/SQLConnection.java
@@ -26,7 +26,7 @@ public class SQLConnection
 	public final String statement = "" +
 	"SELECT id, interaction_geo_latitude, interaction_geo_longitude, twitter_user_location, twitter_user_lang " +
 	"FROM datasift_results " +
-	"WHERE id > 33900000 " +
+	"WHERE id > 36100000 " +
 	//"WHERE country is null " + 
 	"AND (twitter_user_location is not null or interaction_geo_latitude is not null)";  //
 
